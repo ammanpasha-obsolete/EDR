be2ba01a4873f835f1baa2ad146205d562586dda
ammanpasha <amman.pasha@yahoo.com>
2018-01-15 19:01:27 +0500

    a

---
+++ ExcelReaderaltqq/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ExcelReaderaltqq
+{
+    partial class ExcelDataReader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(251, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Excel Sheet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selected Sheet: ";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter Column Name:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Update Tables";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // ExcelDataReader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 218);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "ExcelDataReader";
+            this.Text = "ExcelDataReader - altqq";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ExcelReaderaltqq/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace ExcelReaderaltqq
+{
+    public partial class ExcelDataReader : Form
+    {
+        public ExcelDataReader()
+        {
+            InitializeComponent();
+            label2.Text = "None";
+            groupBox1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Microsoft Excel Worksheet (.xlsx)|*.xlsx";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) 
+            {
+                label2.Text = openFileDialog1.SafeFileName;
+                groupBox1.Visible = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string constr = @"Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + openFileDialog1.FileName + ";Extended Properties='Excel 8.0;HDR=Yes;';";
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = constr;
+            OleDbCommand cmd = new OleDbCommand();
+            cmd.Connection = conn;
+            conn.Open();
+            List<string> ids = new List<string>(); //to store all the IDs fetched from excel sheet.
+            OleDbDataReader dr;
+            cmd.CommandText = "select [" + textBox1.Text +"] from [Sheet1$]";
+            dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                ids.Add(Convert.ToString(dr[0])); //read  from excel sheet
+            }
+            conn.Close();
+
+            //write data to SQL tables
+            string myfilepath = @"C:\Users\Pasha\Documents\ExcelReaderaltqqDB.mdf"; //edit your file path here.
+            String SQLconstr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=" + myfilepath + ";Integrated Security=True;Connect Timeout=30";
+            SqlConnection sqlconn = new SqlConnection();
+            sqlconn.ConnectionString = SQLconstr;
+            SqlCommand sqlcmd = new SqlCommand();
+            sqlcmd.Connection = sqlconn;
+            sqlconn.Open();
+            foreach (string id in ids)
+            {
+                sqlcmd.CommandText = "UPDATE [Table1] set [columnDate] = CONVERT(DATE,GETDATE()), [columnBool] = 1, [columnString]='foo' where [id]='" + id + "'";
+                sqlcmd.ExecuteNonQuery();
+                sqlcmd.CommandText = "UPDATE [Table2] set [columnDate2] = CONVERT(DATE,GETDATE()), [columnBool2] = 1, [columnString2]='bar' where [id]='" + id + "'";
+                sqlcmd.ExecuteNonQuery();
+            }
+            MessageBox.Show("Both tables are updated!");
+            sqlconn.Close();
+
+        }
+    }
+}
